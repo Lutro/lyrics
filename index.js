d558da95bee5134d4cb7c75f8cc8e7372d0779c1
Lutro <stevengamez00@gmail.com>
2019-04-23 21:51:15 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Helene Fischer</title>
+    </head>
+    <body>
+        <h1>Lyrics</h1>
+        <p id="line1" >Der tisch gedeckt, der wein schon kalt</p>
+        <p>Aber du bist noch, nicht da</p>
+        <p id="display" onclick="addNewLine()">hekki</p>
+        <p></p>
+        <p></p>
+        
+        <script src="lyrics.js" ></script>
+    </body>
+    
+</html>
